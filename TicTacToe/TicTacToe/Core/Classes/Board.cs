6c64d0e23c683e05087438947159589a8da31990
alstof <alstof@gmail.com>
2018-12-10 12:31:39 -0800

    Add random cell getter

--- TicTacToe/TicTacToe/Core/Classes/Board.cs
+++ TicTacToe/TicTacToe/Core/Classes/Board.cs
@@ -5,9 +5,18 @@ using System.Threading.Tasks;
 
 namespace TicTacToe.Core.Classes
 {
+    /// <summary>
+    /// Game board
+    /// </summary>
     public class Board
     {
+        /// <summary>
+        /// All cells of the board
+        /// </summary>
         public Cell[] Cells { get; set; }
+        /// <summary>
+        /// All available cells to make move
+        /// </summary>
         public Dictionary<int, Cell> MovesLeft
         {
             get
@@ -15,7 +24,13 @@ namespace TicTacToe.Core.Classes
                 return Cells.Where(cell => cell.Symbol == string.Empty).ToDictionary(cell => cell.Position, cell => cell);
             }
         }
+        /// <summary>
+        /// Dimention of the board (currently hard set to 3 * 3)
+        /// </summary>
         public int Dimension { get; set; }
+        /// <summary>
+        /// Populate a board with cells 
+        /// </summary>
         public void Initialize()
         {
             Cells = new Cell[Dimension];
@@ -25,6 +40,15 @@ namespace TicTacToe.Core.Classes
             {
                 Cells[i] = new Cell() { Position = i };
             }
+        }
+        /// <summary>
+        /// Get any available for a move cell 
+        /// </summary>
+        /// <returns>A cell</returns>
+        public Cell GetRandomAvailableCell()
+        {
+            Random rnd = new Random();
+            return MovesLeft.Values.ToArray()[rnd.Next(0, MovesLeft.Count)];
         }
     }
 }
