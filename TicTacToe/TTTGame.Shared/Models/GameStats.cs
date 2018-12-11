2ff5b05ab138eb6e7c6e4a40768ea64c99be590f
thejamilah <jamilahmcw@gmail.com>
2018-12-11 15:06:35 -0800

    User Story 12 AppDB Scaffold Completed Operations need to be coded still

---
+++ TicTacToe/TTTGame.Shared/Models/GameStats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TTTGame.Shared.Models
+{
+    class GameStats
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Wins")]
+        public int Wins { get; set; }
+
+        [Display(Name = "Losses")]
+        public int Losses { get; set; }
+
+        [Display(Name = "Ties")]
+        public int Ties { get; set; }
+
+        [Display(Name = "AVG Game Time")]
+        [DataType(DataType.Time)]
+        public TimeSpan AvgerageGameTime { get; set; }
+    }
+}
